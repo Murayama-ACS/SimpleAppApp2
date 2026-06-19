c4d96dde33fff494ec7e7d8f63f50270dfba2fe2
User17 <User17@PC-EDU17>
2026-06-19 17:00:09 +0900

    ApplicationBeanのテスト

---
+++ src/main/java/Test/ApplicationBean.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class ApplicationBean {
+
+	public static void main(String[] args) {
+		ApplicationBean appBean = new ApplicationBean();
+		System.out.println(appBean);
+	}
+
+}
